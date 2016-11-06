b92de101062dacc05397f4bb5d97905072266020
Christoph Lukas <lukas@pingworks.de>
2016-11-06 11:03:15 +0100

    splitted exec and run scripts

--- kubernetes-exec.sh
+++ kubernetes-exec.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
 name=$1
-img=$2
-cmd=$3
-override_file=$4
+cmd=$2
 
-if [ -z "$name" -o -z "$img" -o -z "$cmd" ]; then
-  echo "Usage $0 <name> <image> <cmd> [<override-file>]"
+if [ -z "$name" -o -z "$cmd" ]; then
+  echo "Usage $0 <name> <cmd>"
   exit 1
 fi
 if [ -z "$KUBECTL" ]; then
@@ -22,26 +20,15 @@ if [ -z "$NAMESPACE" ]; then
   NAMESPACE="default"
 fi
 
-[ -z "$override_file" ] && override_file="kubernetes-run-overrides.json"
-
 function join_by { local d=$1; shift; echo -n "$1"; shift; printf "%s" "${@/#/$d}"; }
 cmdstring='[ "'$(join_by '", "' $cmd)'" ]'
 
-$KUBECTL run $name \
+$KUBECTL exec -ti $name \
   --server=${KUBE_SERVER} \
   --certificate-authority="/run/secrets/kubernetes.io/serviceaccount/ca.crt" \
   --token="$(</run/secrets/kubernetes.io/serviceaccount/token)" \
   --namespace="$NAMESPACE" \
-  --image=kube-registry.kube-system.svc.cluster.local:5000/${img} \
-  --restart=Never \
-  --overrides="$(sed -e "s;__NAME__;$name;g" \
-	-e "s;__IMAGE__;$img;g" \
-	-e "s;__CMD__;$cmdstring;g" \
-  -e "s;__NAMESPACE__;$NAMESPACE;g" \
-	$(dirname $0)/${override_file})"
-
-echo
-bash $(dirname $0)/wait-for-pod-state.sh "app=${name}" Running 30
+  $cmd
 
 echo "Reading logs from pod $name:"
 echo

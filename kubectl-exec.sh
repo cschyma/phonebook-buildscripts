ad46ad1d2e80e9b697a47569ae1621330df9a3f0
Christoph Lukas <lukas@pingworks.de>
2016-11-06 11:25:19 +0100

    fixed cmd parameter

--- kubectl-exec.sh
+++ kubectl-exec.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 name=$1
-shift
+cmd=$2
 
-if [ -z "$name" -o -z "$*" ]; then
+if [ -z "$name" -o -z "$cmd" ]; then
   echo "Usage $0 <name> <cmd>"
   exit 1
 fi
@@ -28,7 +28,7 @@ $KUBECTL exec -ti $name \
   --certificate-authority="/run/secrets/kubernetes.io/serviceaccount/ca.crt" \
   --token="$(</run/secrets/kubernetes.io/serviceaccount/token)" \
   --namespace="$NAMESPACE" \
-  -- $*
+  -- bash -c "$cmd"
 
 echo "Reading logs from pod $name:"
 echo
